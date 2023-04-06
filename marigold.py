08515e751534e84b5fc2782bfcf7698786d05748
RutgerJTK <rjtkemperman@gmail.com>
2023-04-06 13:35:42 +0200

    some website mehness

--- marigold.py
+++ marigold.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+import numpy as np
+import json
+import sys
+import time 
+
+file = "D:\\Project_IAS\\ProjectCode\\ias_names_big_unedited"
+d = {}
+with open(file) as f:
+    for line in f:
+        (key, val) = line.split(":")
+        val = val.strip("\n")
+        d[(key)] = val
+f.close()
+
+sys.stdout = open("D:\\Project_IAS\\declare.js", "w")
+jsonobj = json.dumps(d)
+print("var jasonstr = '{}'".format(jsonobj))
 
 
-@app.route('/')
-def main_page():
-    return render_template('homepage.html')
+# app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug="True")
+
+# @app.route('/')
+# def main_page():
+#     return render_template('index.html')
+
+# if __name__ == "__main__":
+#     # dict = read_dict()
+#     # dict_to_json(dict)
+#     # time.sleep(3)
+
+#     app.run(debug="True")
 
 
