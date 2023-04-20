32cea4aace21cbba8c9dcbdcffd370d482c2564c
RutgerJTK <rjtkemperman@gmail.com>
2023-04-20 17:06:15 +0200

    html is pain

--- marigold.py
+++ marigold.py
@@ -5,20 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('index.html', species_dict=gen_spec_info_dict )
+    return render_template('index.html', species_dict=gen_spec_info_dict, RA_info_dict=RA_dict )
+
 
 @app.route('/species/<species_name>')
-def species_page(species_name, RA_dict):
+def species_page(species_name):
     selected_species = request.args.get('selected_species')
     species_info = gen_spec_info_dict.get(species_name)
-    species_risk_assessments = request.args.get('selected_ra_info')
-    species_info = RA_dict.get(species_risk_assessments)
+    # species_risk_info = request.args.get('species_risk_info')
+    species_risk_info = RA_dict.get(names_dict[species_name])
     print(species_name)
     print(species_info)
-    return render_template('species.html', species_risk_info=RA_dict, species=species_info, selected_species=selected_species)
+    
+    return render_template('species.html', species_ra_info=RA_dict, species=species_info, selected_species=selected_species, names_dict=names_dict)
+
 
 if __name__ == "__main__":
     names_dict, static_url_path, gen_spec_info_dict, RA_dict = marigold_boot_req.main()
+    print(names_dict)
     app.static_url_path=static_url_path
     app.config['NAMES_DICT'] = names_dict
     app.run()
