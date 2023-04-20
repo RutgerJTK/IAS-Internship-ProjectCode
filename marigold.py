0f811b72730383a64947bc43d96c592bf6f40c8d
RutgerJTK <rjtkemperman@gmail.com>
2023-04-20 13:08:42 +0200

    small stuff

--- marigold.py
+++ marigold.py
@@ -1,24 +1,21 @@
 from flask import Flask, render_template, request
-import numpy as np
-import json
-import sys
-import time 
-import os
-import re
-import csv
 import marigold_boot_req
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('index.html', my_dict=gen_spec_info_dict )
+    return render_template('index.html', species_dict=gen_spec_info_dict )
 
 @app.route('/species/<species_name>')
-def species_page(species_name):
+def species_page(species_name, RA_dict):
     selected_species = request.args.get('selected_species')
     species_info = gen_spec_info_dict.get(species_name)
-    return render_template('species.html', species=species_info, selected_species=selected_species)
+    species_risk_assessments = request.args.get('selected_ra_info')
+    species_info = RA_dict.get(species_risk_assessments)
+    print(species_name)
+    print(species_info)
+    return render_template('species.html', species_risk_info=RA_dict, species=species_info, selected_species=selected_species)
 
 if __name__ == "__main__":
     names_dict, static_url_path, gen_spec_info_dict, RA_dict = marigold_boot_req.main()
