4a48fa7fc052c59d20fd001ff96d0dfae6c7ce61
RutgerJTK <smartrutger@gmail.com>
2023-06-29 19:53:56 +0200

    last analysis and plotting done

--- G_Trends_Plotter_Species.py
+++ G_Trends_Plotter_Species.py
@@ -25,32 +25,32 @@ import seaborn as sns
 sns.set_theme()
 
 def plot_observations_stats(provinces, timestamps, file):
-    save_path = "D:\\Project_IAS\\Plotted_stats\\Province_counts\\"
+    save_path = "D:\\Project_IAS\\Plotted_stats\\Province_counts_20y\\"
     save_pie = "province_obs_counts_{}".format(file)
     provinces_set = reduce(lambda re, x: re+[x] if x not in re else re, provinces, []) # Shows order of which each species was discovered/observed chronologically in the Netherlands. 
     cols = ["#4c88bf","#f1d409", "#04a86b", "#f8151c", "#d79004", "#87edc9", "#b62bb6", "#0f9d12", "#7e05fe", "#a06261", "#6c9a1f", "#d79004"]
     if len(provinces_set) > 0:
         count_provinces = pd.Series(provinces).value_counts()    
         list_prov_counts = count_provinces.tolist()
-        # plt.figure(figsize = (10,7))
-        # plt.title("Frequency of species observations per each province since {}".format(timestamps[-1]),  bbox={'facecolor':'1', 'pad':1})
+        plt.figure(figsize = (10,7))
+        plt.title("Frequency of species observations per each province since {}".format(timestamps[-1]),  bbox={'facecolor':'1', 'pad':1})
 
-        # plt.pie(list_prov_counts, startangle=90,
-        #             wedgeprops = {"edgecolor" : "black", "linewidth": 4, 'width':1,
-        #                 'linewidth': 0.5,
-        #                 'antialiased': True},
-        #                 autopct= '%1.1f%%',
-        #                 colors=cols)
-        # plt.axis('equal')
-        # labels = provinces_set
-        # labels = [f'{l}: {s}' for l, s in zip(labels, list_prov_counts)]
-        # plt.legend(bbox_to_anchor=(-0.15, 0.25), loc='lower left', labels=labels, title="Counts per province")
+        plt.pie(list_prov_counts, startangle=90,
+                    wedgeprops = {"edgecolor" : "black", "linewidth": 4, 'width':1,
+                        'linewidth': 0.5,
+                        'antialiased': True},
+                        autopct= '%1.1f%%',
+                        colors=cols)
+        plt.axis('equal')
+        labels = provinces_set
+        labels = [f'{l}: {s}' for l, s in zip(labels, list_prov_counts)]
+        plt.legend(bbox_to_anchor=(-0.15, 0.25), loc='lower left', labels=labels, title="Counts per province")
         
-        # # plt.show()
-        # plt.savefig((save_path + save_pie), dpi='figure', format=None,
-        #     bbox_inches=None, pad_inches=0.1,
-        #     facecolor='auto', edgecolor='auto',
-        #     backend=None)
+        # plt.show()
+        plt.savefig((save_path + save_pie), dpi='figure', format=None,
+            bbox_inches=None, pad_inches=0.1,
+            facecolor='auto', edgecolor='auto',
+            backend=None)
         plt.close()
         print("here")
 
@@ -86,19 +86,19 @@ def plot_obs_cummulative(timestamps, indiv_nrs_list, file):
     # Assuming you have the last data point coordinates (last_x, last_y) in your plot
     last_x = timestamps[-1]
     last_y = cummul_list[-1]
-    # try:
-    #     # Calculate the step size for the x-axis ticks
-    #     percentiles = np.arange(10, 100, 10)
-    #     indices = np.percentile(np.arange(len(timestamps)), percentiles)
-    #     x_ticks = [timestamps[int(index)] for index in indices]
+    try:
+        # Calculate the step size for the x-axis ticks
+        percentiles = np.arange(10, 100, 10)
+        indices = np.percentile(np.arange(len(timestamps)), percentiles)
+        x_ticks = [timestamps[int(index)] for index in indices]
 
-    #     # Set the x-axis ticks
-    #     plt.xticks(x_ticks, rotation=45)
+        # Set the x-axis ticks
+        plt.xticks(x_ticks, rotation=45)
 
-    # except ZeroDivisionError:
-    #     plt.xticks([timestamps[0], timestamps[-1]], rotation=45)
-    #     ax = plt.gca()
-    #     ax.invert_xaxis()  # Invert the x-axis ticks
+    except ZeroDivisionError:
+        plt.xticks([timestamps[0], timestamps[-1]], rotation=45)
+        ax = plt.gca()
+        ax.invert_xaxis()  # Invert the x-axis ticks
 
     # Annotate the last data point with a number
     plt.annotate(f'{last_y}', (last_x, last_y), xytext=(5, 10),
@@ -122,11 +122,11 @@ def main_trends_plotter(): # Stats to do: google trends vs
             print("Name: " + gen_info[0])
             if len(element_list) > 0:
                 obs_dict = Waarnemingen_attributes.fill_obs_dict(element_list)
-            # plot_observations_stats(provinces, timestamps, file)    # Don't remove this; it won't overwrite the figure if it is already saved under that name. ToDo: fix this. 
-            if len(indiv_nrs_list) > 0:
-                plot_obs_cummulative(timestamps, indiv_nrs_list, file)
-            else:
-                print("0")
+            plot_observations_stats(provinces, timestamps, file)    # Don't remove this; it won't overwrite the figure if it is already saved under that name. ToDo: fix this. 
+            # if len(indiv_nrs_list) > 0:
+            #     plot_obs_cummulative(timestamps, indiv_nrs_list, file)
+            # else:
+            #     print("0")
 
 
 if __name__ == "__main__":
